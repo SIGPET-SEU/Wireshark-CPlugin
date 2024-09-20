92c74272a84f1bae122d91b1a81d6a0e71fffa42
wesly2000 <ylx.seu@foxmail.com>
2024-09-20 09:35:45 +0800

    Fix bugs.
    Fix the bug that VMess Request could not be recognized. The statement
    `!conv_data->auth` should not be used in the outer if, since the inner
    codes would be skipped in the second scan of Wireshark, leading to the
    overwrite on column info.

--- vmess/packet-vmess.c
+++ vmess/packet-vmess.c
@@ -123,31 +123,10 @@ proto_reg_handoff_vmess(void)
 
 static int
 dissect_vmess_request(tvbuff_t* tvb, packet_info* pinfo, proto_tree* tree _U_, void* data _U_) {
-    conversation_t* conversation;
-    vmess_conv_t* conv_data;
-
-    /* get conversation, create if necessary*/
-    conversation = find_or_create_conversation(pinfo);
-
-    /* get associated state information, create if necessary */
-    conv_data = (vmess_conv_t*)conversation_get_proto_data(conversation, proto_vmess);
-    if (!conv_data) {
-        conv_data = wmem_new0(wmem_file_scope(), vmess_conv_t);
-        conversation_add_proto_data(conversation, proto_vmess, conv_data);
-    }
-    if (!conv_data->reassembly_info) {
-        conv_data->reassembly_info = streaming_reassembly_info_new();
-    }
-
     col_set_str(pinfo->cinfo, COL_INFO, "VMESS Request");
     proto_item* ti = proto_tree_add_item(tree, proto_vmess, tvb, 0, -1, ENC_NA);
     proto_tree* vmess_tree = proto_item_add_subtree(ti, ett_vmess);
     proto_tree_add_item(vmess_tree, hf_vmess_request_auth, tvb, 0, 16, ENC_BIG_ENDIAN);
-
-    reassemble_streaming_data_and_call_subdissector(tvb, pinfo, 0, tvb_reported_length_remaining(tvb, 0),
-        vmess_tree, tree, proto_vmess_streaming_reassembly_table,
-        conv_data->reassembly_info, get_virtual_frame_num64(tvb, pinfo, 0), vmess_request_handle,
-        proto_tree_get_parent_tree(tree), NULL, "VMess", &msg_frag_items, hf_msg_body_segment);
 
     return 0;
 }
@@ -190,10 +169,7 @@ dissect_vmess_response_pdu(tvbuff_t* tvb, packet_info* pinfo, proto_tree* tree _
 
     next_tvb = tvb_new_subset_remaining(tvb, 40);
 
-    //tls_handle = find_dissector("tls");
-
     if (next_tvb) {
-        //add_new_data_source(pinfo, next_tvb, "vmess segment data");
         reassemble_streaming_data_and_call_subdissector(next_tvb, pinfo, 0, tvb_reported_length_remaining(next_tvb, 0),
             vmess_tree, tree, proto_vmess_streaming_reassembly_table,
             conv_data->reassembly_info, get_virtual_frame_num64(next_tvb, pinfo, 0), tls_handle,
@@ -239,11 +215,7 @@ dissect_vmess_data_pdu(tvbuff_t* tvb, packet_info* pinfo, proto_tree* tree _U_, 
 
     next_tvb = tvb_new_subset_remaining(tvb, 2);
 
-    //tls_handle = find_dissector("tls");
-
-
     if (next_tvb) {
-        //add_new_data_source(pinfo, next_tvb, "vmess segment data");
         reassemble_streaming_data_and_call_subdissector(next_tvb, pinfo, 0, tvb_reported_length_remaining(next_tvb, 0),
             vmess_tree, tree, proto_vmess_streaming_reassembly_table,
             conv_data->reassembly_info, get_virtual_frame_num64(next_tvb, pinfo, 0), tls_handle,
@@ -281,17 +253,17 @@ dissect_vmess(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *dat
 
     bool is_request = false;
 
-    if (!conv_data->auth) { /* If the auth of the conversation is not NULL, there is no need for auth check */
-        if (tvb_reported_length(tvb) > 61) { /* Minimum VMess request length */
-            gchar* tmp_auth = (gchar*)g_malloc((VMESS_AUTH_LENGTH + 1) * sizeof(gchar));
-            tvb_get_raw_bytes_as_string(tvb, 0, tmp_auth, (VMESS_AUTH_LENGTH + 1));
-            if (char_array_eq(auth, tmp_auth, VMESS_AUTH_LENGTH)) {
-                conv_data->auth = wmem_strndup(pinfo->pool, tmp_auth, VMESS_AUTH_LENGTH);
-                is_request = true;
+    
+    if (tvb_reported_length(tvb) > 61) { /* Minimum VMess request length */
+        gchar* tmp_auth = (gchar*)g_malloc((VMESS_AUTH_LENGTH + 1) * sizeof(gchar));
+        tvb_get_raw_bytes_as_string(tvb, 0, tmp_auth, (VMESS_AUTH_LENGTH + 1));
+        if (char_array_eq(auth, tmp_auth, VMESS_AUTH_LENGTH)) {
+            if (!PINFO_FD_VISITED(pinfo) && !conv_data->auth) { /* If the auth of the conversation is not NULL, there is no need for auth check */
+                conv_data->auth = "\xfc\xc1\x8b\x89\x42\xc6\x70\xfd\xcb\x17\xe3\xb0\x7f\x72\xf2\x7f";
             }
+            is_request = true;
         }
     }
-    
 
     if (is_request) {
         dissect_vmess_request(tvb, pinfo, tree, NULL);
@@ -302,15 +274,10 @@ dissect_vmess(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *dat
     gint pos = tvb_find_TLS_signiture(tvb);
     if (pos == 40) is_response = true;
 
-    //conversation = find_conversation_pinfo(pinfo, 0);
-    //if (conversation) {
-    //    conv_data = (vmess_conv_t*)conversation_get_proto_data(conversation, proto_vmess);
-    //}
-
-    //save_port_type = pinfo->ptype;
-    //pinfo->ptype = PT_NONE;
-    //save_can_desegment = pinfo->can_desegment;
-    //pinfo->can_desegment = pinfo->saved_can_desegment;
+    save_port_type = pinfo->ptype;
+    pinfo->ptype = PT_NONE;
+    save_can_desegment = pinfo->can_desegment;
+    pinfo->can_desegment = pinfo->saved_can_desegment;
 
     if (conv_data && is_response && pinfo->num > conv_data->startframe) {
         tcp_dissect_pdus(tvb, pinfo, tree, vmess_desegment,
@@ -324,8 +291,8 @@ dissect_vmess(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *dat
             dissect_vmess_data_pdu, data);
     }
 
-    //pinfo->ptype = save_port_type;
-    //pinfo->can_desegment = save_can_desegment;
+    pinfo->ptype = save_port_type;
+    pinfo->can_desegment = save_can_desegment;
 
     return tvb_reported_length(tvb);
 }

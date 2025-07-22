c5cb7510c896fe81123e03f00c8a41ee5ccb41ac
wesly2000 <ylx.seu@foxmail.com>
2025-07-22 17:42:35 +0800

    misc: update port range

--- vmess/packet-vmess.h
+++ vmess/packet-vmess.h
@@ -31,7 +31,7 @@ const true_false_string tfs_set_notset_vmess = { "Set", "Not set" };
  * Change single port to port range, the usage is mentioned in packet-xml.c.
  * Although packet-http.c also records the usage, it seems to be quite complicated.
  */
-#define VMESS_TCP_PORT_RANGE "20332, 20002, 10006, 20016, 20054"
+#define VMESS_TCP_PORT_RANGE "20332, 20002, 10006, 20016, 20054, 10008"
 
 #define VMESS_AUTH_LENGTH (guint) 16
 #define VMESS_CHECKSUM_LENGTH (guint) 4
